7cfb69ebd578f3a5ac4ee91cf4841bfc9c4a676d
emmerichdev <emmerichhbrowne@gmail.com>
2026-01-18 15:45:12 -0600

    feat: fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,19 +6,7 @@ plugins {
 group = "dev.solarion"
 version = "1.0-SNAPSHOT"
 
-val repoUser = System.getenv("REPO_USERNAME") ?: project.findProperty("repoUser") as String? ?: project.findProperty("mavenUser") as String? ?: ""
-val repoPass = System.getenv("REPO_PASSWORD") ?: project.findProperty("repoPass") as String? ?: project.findProperty("mavenPass") as String? ?: ""
-
-
 repositories {
-    maven {
-        url = uri("https://repo.solarion.dev/repository/maven-snapshots/")
-        name = "SolarionInteractive"
-        credentials {
-            username = repoUser
-            password = repoPass
-        }
-    }
     mavenCentral()
 }
 
@@ -26,12 +14,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    if(System.getenv("CI_COMMIT_SHORT_SHA") != null) {
-        compileOnly("com.hypixel.hytale:Server:2026.01.17-4b0f30090")
-    } else {
-        implementation("com.hypixel.hytale:Server:2026.01.17-4b0f30090")
-    }
+    implementation(files("${System.getProperty("user.home")}/AppData/Roaming/Hytale/install/release/package/game/latest/Server/HytaleServer.jar"))
 }
 
 tasks.test {
